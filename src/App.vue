50221b6f1040d7b31d05aeff2cefc2d4af904ce2
Jose Flor Gaston <joseflorgaston@gmail.com>
2022-05-29 21:28:34 -0400

    startup page

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header></Header>
+    <HeroSection> </HeroSection>
+    <MainVue></MainVue>
+    <FooterVue></FooterVue>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import HeroSection from './components/HeroSection.vue';
+import MainVue from './components/Main.vue';
+import FooterVue from './components/Footer.vue';
+</script>
+
+<style>
+@import 'assets/css/style.css';
+@import 'assets/vendor/aos/aos.css';
+@import 'assets/vendor/bootstrap/css/bootstrap.min.css';
+@import 'assets/vendor/bootstrap-icons/bootstrap-icons.css';
+@import 'assets/vendor/boxicons/css/boxicons.min.css';
+@import 'assets/vendor/glightbox/css/glightbox.min.css';
+@import 'assets/vendor/remixicon/remixicon.css';
+@import 'assets/vendor/swiper/swiper-bundle.min.css';
+
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <main id="main">
+      <ClientSection> </ClientSection>
+      <AboutUsSection> </AboutUsSection>
+      <WhyUsSection> </WhyUsSection>
+      <SkillsSectionVue> </SkillsSectionVue>
+      <ServicesSectionVue></ServicesSectionVue>
+      <CTASectionVue> </CTASectionVue>
+      <PortfolioSectionVue></PortfolioSectionVue>
+      <TeamSection></TeamSection>
+      <PricingSectionVue></PricingSectionVue>
+      <QuestionsSectionVue></QuestionsSectionVue>
+      <ContactSectionVue></ContactSectionVue>
+  </main>
+</template>
+
+<script setup lang="ts">
+import ClientSection from './main/ClientSection.vue';
+import AboutUsSection from './main/AboutUsSection.vue';
+import WhyUsSection from './main/WhyUsSection.vue';
+import SkillsSectionVue from './main/SkillsSection.vue';
+import ServicesSectionVue from './main/ServicesSection.vue';
+import CTASectionVue from './main/CTASection.vue';
+import PortfolioSectionVue from './main/PortfolioSection.vue';
+import TeamSection from './main/TeamSection.vue'
+import PricingSectionVue from './main/PricingSection.vue';
+import QuestionsSectionVue from './main/QuestionsSection.vue';
+import ContactSectionVue from './main/ContactSection.vue';
+</script>
+
+<style>
+
+</style>
